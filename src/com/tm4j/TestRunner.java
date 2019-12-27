db3c5d5a73023c81bcb14809b03854e9fe588339
Suree1988 <44668926+Suree1988@users.noreply.github.com>
2019-12-28 00:54:02 +0100

    Update TestRunner.java

--- src/com/tm4j/TestRunner.java
+++ src/com/tm4j/TestRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;		
 
 @RunWith(Cucumber.class)				
-@CucumberOptions(features= {"bdd-export3958336676467726768"},
+@CucumberOptions(features= {"bdd-export3958336676467726768/TM-T2.feature"},
 glue= {"com.tm4j"},
 format= {"junit:Test-soaui_report.xml","json:target/cucumber/tm4j_result.json"})		
 
