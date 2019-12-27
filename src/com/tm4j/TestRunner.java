80c07840708e0a3ac5e1d5ee0535adbc5fb57862
Suree1988 <sureerat1988@gmail.com>
2019-12-28 00:00:08 +0100

    Change the json name

--- src/com/tm4j/TestRunner.java
+++ src/com/tm4j/TestRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)				
 @CucumberOptions(features= {"bdd-export3958336676467726768"},
 glue= {"com.tm4j"},
-format= {"junit:Test-soaui_report.xml","json:Test-soaui_report.json"})		
+format= {"junit:Test-soaui_report.xml","json:tm4j_result.json.json"})		
 
 public class TestRunner {
 
