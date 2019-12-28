b1f04b7dadd50b50b9cef542dfa20215d70ee942
Suree1988 <sureerat1988@gmail.com>
2019-12-28 03:12:06 +0100

    TM4J_2

--- src/testCode/TestRunner.java
+++ src/testCode/TestRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;		
 
 @RunWith(Cucumber.class)				
-@CucumberOptions(features= {"C:\\Users\\suree\\eclipse-workspace\\SoapUI2Jira_TM4J\\bdd-export5718803273015028755"},
+@CucumberOptions(features= {"bdd-export5718803273015028755"},
 glue= {"testCode"},
 tags= {"@TestCaseKey=TM-T2"},
 format= {"junit:target/tm4j/Test-soaui_report.xml","json:target/tm4j/Test-soaui_report.json"})		
